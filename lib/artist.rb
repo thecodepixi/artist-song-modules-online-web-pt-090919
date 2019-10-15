0efd63f8e09af3c2755b32139c306f3201bd6425
Emily Harber <emily.a.harber@gmail.com>
2019-10-15 23:39:32 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,6 +3,7 @@ require 'pry'
 class Artist
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
+  include Memorable::InstanceMethods 
   include Paramable::InstanceMethods 
   
   attr_accessor :name
