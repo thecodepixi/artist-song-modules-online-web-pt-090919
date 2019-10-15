43ca774350b84d649dc4f720f36c3fba5a98078e
Emily Harber <emily.a.harber@gmail.com>
2019-10-15 23:21:06 +0000

    Automatically backed up by Learn

--- concerns/memorable.rb
+++ concerns/memorable.rb
@@ -1,9 +1,11 @@
 module Memorable
-  def reset_all
-    all.clear
-  end
+  def ClassMethods 
+    def reset_all
+      all.clear
+    end
 
-  def count
-    all.count
-  end
+    def count
+      all.count
+    end
+  end 
 end 
